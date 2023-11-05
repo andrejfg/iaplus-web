d67b4fd2d856fe7253176d591d2891476e215c57
André Felipe Guimarães <65873168+andrejfg@users.noreply.github.com>
2023-11-05 09:42:56 -0300

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen items-center justify-center bg-black p-24">
+      <h1 className="text-3xl text-white">Hello World!</h1>
+    </main>
+  )
+}
